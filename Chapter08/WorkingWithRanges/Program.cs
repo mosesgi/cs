d31c7d93ac8b0a3759226a0436ff545a3ab3e9ed
Moses Ji <mji@arenasolutions.com>
2021-05-13 17:16:15 +0800

    chapter08

---
+++ Chapter08/WorkingWithRanges/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using static System.Console;
+
+namespace WorkingWithRanges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Samantha Jones";
+            int lengthOfFirst = name.IndexOf(' ');
+            int lengthOfLast = name.Length - lengthOfFirst - 1;
+            string firstName = name.Substring(0, lengthOfFirst);
+            string lastName = name.Substring(lengthOfFirst + 1, lengthOfLast);
+            WriteLine($"First name: {firstName}, Last name: {lastName}");
+            ReadOnlySpan<char> nameAsSpan = name.AsSpan();
+            var firstNameSpan = nameAsSpan[0..lengthOfFirst];
+            var lastNameSpan = nameAsSpan[^lengthOfLast..^0];       // ^ stands for "from end"
+            WriteLine("First name: {0}, Last name: {1}", firstNameSpan.ToString(), lastNameSpan.ToString());
+        }
+    }
+}
