909215f70c9bb8a113507d2d1292e95edd993117
Moses Ji <mji@arenasolutions.com>
2021-05-14 18:23:27 +0800

    chapter 10 till Hashing Data

---
+++ Chapter10/EncryptionApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Security.Cryptography;
+using Packt.Shared;
+using static System.Console;
+
+namespace EncryptionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Enter a message that you want to encrypt: ");
+            string message = ReadLine();
+            Write("Enter a password: ");
+            string password = ReadLine();
+            string cryptoText = Protector.Encrypt(message, password);
+            WriteLine($"Encrypted text: {cryptoText}");
+            Write("Enter the password: ");
+            string password2 = ReadLine();
+            try
+            {
+                string clearText = Protector.Decrypt(cryptoText, password2);
+                WriteLine($"Decrypted text: {clearText}");
+            }
+            catch (CryptographicException ex)
+            {
+                WriteLine("{0}\n More details: {1}", "You entered the wrong password!", ex.Message);
+            }
+            catch (Exception ex)
+            {
+                WriteLine("Non-cryptographic exception: {0}, {1}", ex.GetType().Name, ex.Message);
+            }
+        }
+    }
+}
